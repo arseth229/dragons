915ec4b80f8bed667d5e25afffeaab275737a792
arseth229 <146048786+arseth229@users.noreply.github.com>
2024-03-12 15:44:09 -0500

    dragon-spec passing

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,29 @@
-// Your code here 
+class Dragon {
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    let dragonarr = [];
+    for (let dragon of dragons) {
+      console.log(dragon);
+      let name = dragon.name
+      dragonarr.push(name);
+    }
+    return dragonarr;
+  }
+}
+
+const tyler = new Dragon('tyler', 'green')
+
+Dragon.getDragons(tyler);
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
