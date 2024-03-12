2dc4beca9f6ed2d06ee2516f4f7ac35c579b6f6d
Ali <ahsana1045@gmail.com>
2024-03-12 14:15:17 -0700

    FriendlyDragon Complete

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,36 @@
-// Your code here 
+// Your code here
+const Dragon = require ('./dragon')
+
+class FriendlyDragon extends Dragon{
+    constructor(name, color, lifeGoals, friend){
+        super(name, color)
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+    hasLifeGoals(){
+        for (let i = 0; i < this.lifeGoals.length; i++) {
+            const ele = this.lifeGoals[i];
+            console.log(`${this.name} likes to ${ele}`)
+
+        }
+    }
+    helpsPeople(){
+        return `${this.name} helps their friend ${this.friend}`
+    }
+}
+
+const puff = new FriendlyDragon(
+    "Puff",
+    "green",
+    [
+      "live by the sea",
+      "frolick in the autumn mist",
+      "help small children"
+    ],
+    "Jackie Paper"
+  );
+
+puff.hasLifeGoals()
+puff.helpsPeople()
+
+module.exports = FriendlyDragon
